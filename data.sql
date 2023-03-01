ea490a6bb82e021fa8dfd35d6c006606d2e4457c
Salwa99 <s.ritage99@gmail.com>
2023-03-01 07:33:58 +0100

    Insert data into the table

--- data.sql
+++ data.sql
@@ -1,5 +1,4 @@
-/* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES  (1, 'Agumon', 'Feb 3, 2020', 10.23, 'true', 0 ),
+                            (2, 'Gabumon', 'Nov 15, 2018', 8, 'true', 2),
+                            (3, 'Pikachu', 'Jan 7, 2021', 15.04, 'false', 1),
+                            (4, 'Devimon', 'May 12, 2017', 11, 'true', 5);
