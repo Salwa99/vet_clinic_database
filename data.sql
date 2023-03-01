6af195d15c89ca7c0d3f11d9a6e592c95e007b25
Salwa99 <s.ritage99@gmail.com>
2023-03-01 16:00:15 +0100

    Insert data into the table

--- data.sql
+++ data.sql
@@ -1,4 +1,12 @@
 INSERT INTO animals VALUES  (1, 'Agumon', 'Feb 3, 2020', 0, 'true', 10.23 ),
                             (2, 'Gabumon', 'Nov 15, 2018', 2, 'true', 8),
                             (3, 'Pikachu', 'Jan 7, 2021', 1, 'false', 15.04),
-                            (4, 'Devimon', 'May 12, 2017', 5, 'true', 11);
+                            (4, 'Devimon', 'May 12, 2017', 5, 'true', 11),
+                            (5, 'Charmander', 'Feb 8, 2020', 0, 'false', -11),
+                            (6, 'Plantmon', 'Nov 15 2021', 2, 'true', -5.7),
+                            (7, 'Squirtle', 'Apr 2, 1993', 3, 'false', -12.13),
+                            (8, 'Angemon', 'Jun 12, 2005', 1, 'true', -45),
+                            (9, 'Boarmon', 'Jun 7, 2005', 7, 'true', 20.4),
+                            (10, 'Blossom', 'Oct 13, 1998', 3, 'true', 17),
+                            (11, 'Ditto', 'May 14, 2022', 4, 'true', 22)
+                            ;
