40e35319e07be32fa41cff1d6bb1f03866f63536
Salwa99 <s.ritage99@gmail.com>
2023-03-07 15:29:56 +0100

    Create table invoices_items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -25,3 +25,14 @@ generated_at TIMESTAMP,
 payed_at TIMESTAMP,
 CONSTRAINT FK_medical_history FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id)
 ); 
+
+create table invoices_items (
+id SERIAL PRIMARY KEY,
+unit_price DECIMAL,
+quantity INT,
+total_price DECIMAL,
+invoice_id INT,
+treatment_id INT,
+CONSTRAINT FK_invoice FOREIGN KEY (invoice_id) REFERENCES invoices (id),
+CONSTRAINT FK_treatment FOREIGN KEY (treatment_id) REFERENCES treatments (id)
+);
