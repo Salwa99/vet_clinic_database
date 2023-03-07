d903f4b3004bcad8f014b2557bb5eaa7a5b57f0f
Salwa99 <s.ritage99@gmail.com>
2023-03-07 15:28:41 +0100

    Create table medical histrories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,10 @@ name VARCHAR(100),
 date_of_birth DATE
 );
 
+create table medical_histories (
+id SERIAL PRIMARY KEY,
+admitted_at TIMESTAMP,
+patient_id INT,
+status VARCHAR(100),
+CONSTRAINT FK_patient FOREIGN KEY (patient_id) REFERENCES patients(id)
+);
