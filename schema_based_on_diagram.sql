738024b335177a4336747b87efa695376fe832ab
Salwa99 <s.ritage99@gmail.com>
2023-03-07 15:29:11 +0100

    Create table treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,3 +11,9 @@ patient_id INT,
 status VARCHAR(100),
 CONSTRAINT FK_patient FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
+
+create table treatments (
+id SERIAL PRIMARY KEY,
+type VARCHAR(100),
+name VARCHAR(100)
+);
